88c0a9163e9fb6e1647501d6ccd56786da5b61e6
JustKiddingBaby <yangjing9611@foxmail.com>
2018-01-31 15:19:43 +0800

    :hammer:remove leakcanary.

--- app/src/main/java/com/yj/app/App.java
+++ app/src/main/java/com/yj/app/App.java
@@ -1,13 +1,11 @@
 package com.yj.app;
 
 import android.app.Application;
-import com.squareup.leakcanary.LeakCanary;
 
 public class App extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
-    LeakCanary.install(this);
   }
 }
