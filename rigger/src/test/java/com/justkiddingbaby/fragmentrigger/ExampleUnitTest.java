acc5e8bec7afa8024cbf66b9d46cc41e240e03b2
Yeoh <yangjing9611@foxmail.com>
2026-02-01 21:03:52 +0800

    Add rigger/src/test/java/com/justkiddingbaby/fragmentrigger/ExampleUnitTest.java

---
+++ rigger/src/test/java/com/justkiddingbaby/fragmentrigger/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+package com.justkiddingbaby.fragmentrigger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example unit test for FragmentRigger library.
+ * 
+ * This test demonstrates the basic unit testing setup.
+ */
+public class ExampleUnitTest {
+    
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+    
+    @Test
+    public void testLibraryInitialization() {
+        // Test basic library functionality
+        assertTrue("Library should be initialized correctly", true);
+    }
+}
